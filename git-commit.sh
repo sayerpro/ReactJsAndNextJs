5c690f79629a432906c5727eff2704a014313606
TheSrSmith <sayerpro.op7@gmail.com>
2023-05-13 19:06:53 -0500

    Actualización archivo bash

--- git-commit.sh
+++ git-commit.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+current_branch=$(git branch --show-current)
+
 git add .
 read -p "Enter commit message: " message
 git commit -m "$message"
-git pull origin develop
-git push origin develop
+git pull origin $current_branch
+git push origin $current_branch
 git checkout master
 git pull origin master
-git merge develop
+git merge $current_branch
 git push origin master
-git checkout develop
+git checkout $current_branch
